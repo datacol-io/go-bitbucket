db646dc141f96718127a1c0bcd96680dec4d2ed6
dinesh <dinesh1042@gmail.com>
2019-06-29 12:30:31 +0530

    add GetDiffStat API

--- diff.go
+++ diff.go
@@ -9,6 +9,11 @@ func (d *Diff) GetDiff(do *DiffOptions) (interface{}, error) {
 	return d.c.execute("GET", urlStr, "")
 }
 
+func (d *Diff) GetDiffStat(do *DiffOptions) (interface{}, error) {
+	urlStr := d.c.requestUrl("/repositories/%s/%s/diffstat/%s", do.Owner, do.Repo_slug, do.Spec)
+	return d.c.execute("GET", urlStr, "")
+}
+
 func (d *Diff) GetPatch(do *DiffOptions) (interface{}, error) {
 	urlStr := d.c.requestUrl("/repositories/%s/%s/patch/%s", do.Owner, do.Repo_slug, do.Spec)
 	return d.c.execute("GET", urlStr, "")
